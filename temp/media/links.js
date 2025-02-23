7d6b5f9a0bd234bdbf3077fa512775d308404585
BLUEDEMON <bolaolat3000@gmail.com>
2025-02-23 13:26:16 +0100

    Add files via upload

--- temp/media/links.js
+++ temp/media/links.js
@@ -1,1 +1,10 @@
-
+exports.vnMenu = [
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3",
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3",
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3"
+]
+exports.images = [
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg'
+    ];
