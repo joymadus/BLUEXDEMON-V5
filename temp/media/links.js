39e61c18634f3742dfd8b9314214ad0a78493264
BLUEDEMON <bolaolat3000@gmail.com>
2025-02-23 13:07:33 +0100

    Add files via upload

--- temp/media/links.js
+++ temp/media/links.js
@@ -1,17 +1,10 @@
 exports.vnMenu = [
-"https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/audio1.mp3",
-"https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/audio1.mp3",
-"https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/audio1.mp3"
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3",
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3",
+"https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3"
 ]
 exports.images = [
-        'https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/menu1.jpg',
-        'https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/menu2.jpg',
-        'https://huggingface.co/spaces/API-X/BOT-BASE/resolve/main/menu3.jpg'
-    ];
-exports.zoroImages = [
-        'https://wallpapercave.com/wp/wp13833689.jpg',
-        'https://wallpapercave.com/wp/wp12456894.jpg',
-        'https://wallpapercave.com/wp/wp13337959.jpg',
-        'https://wallpapercave.com/wp/wp11267746.jpg',
-        'https://wallpapercave.com/wp/wp13833690.jpg'
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg'
     ];
