7951cde938198509016b946e7c03c3f06d8686b9
BLUEDEMON <bolaolat3000@gmail.com>
2025-02-23 13:42:59 +0100

    Update links.js

--- temp/media/links.js
+++ temp/media/links.js
@@ -4,7 +4,7 @@ exports.vnMenu = [
 "https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.mp3"
 ]
 exports.images = [
-        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
-        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg',
-        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/Jinwoo_Anime_Season_2%20(1).jpg'
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.jpg',
+        'https://huggingface.co/spaces/API-XX/TEST/resolve/main/Links/menu.jpg'
     ];
